e626c078a4b88be8817d6d91a6e2d7b94281cefa
Pradeep <pradeep.voolla@votarytech.com>
2017-08-16 18:50:41 +0530

    Corected main.c

--- main_prog.c
+++ main_prog.c
@@ -2,14 +2,4 @@
 int main(void)
 {
 	int val1,val2;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-	val1=5,val2=10;
-	printf("Val1 is %d Val2 is %d\n",val1,val2);
->>>>>>> child_branch
-=======
-	val1=5,val2=10;
-	printf("Val1 is %d Val2 is %d\n",val1,val2);
->>>>>>> newbranch
 }
